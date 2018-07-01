38412868e6a083303480a704803abe9314c5ad1a
KJ <kernel1983@gmail.com>
2018-07-01 22:01:39 +0800

    simulation script

---
+++ G.py
@@ -0,0 +1,10 @@
+import random
+
+nodes = list(range(1,100))
+
+print("digraph G {")
+for node in nodes:
+    to = random.choice(list(set(nodes) - set([node])))
+    print("%s -> %s;" %(node, to))
+
+print("}")
