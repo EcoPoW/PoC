814c367d239ac000298760d395de261eafae769b
EFFYLYX <effyli1997@gmail.com>
2018-07-03 22:50:19 +0800

    process and server

--- node program/node_control.py
+++ node program/node_control.py
@@ -1,3 +1,11 @@
 import subprocess
-q1 = subprocess.call(["python","Node.py","--port","9090"])
-q2 = subprocess.call(["python","Node.py","--port","9010"])
+# q1 = subprocess.call(["python","node.py","--port","9090","--end","0"])
+# q2 = subprocess.call(["python","node.py","--port","9010","--end","0"])
+# q3 = subprocess.call(["python","node.py","--port","8080","--end","1"])
+
+for i in range(0,10,1):
+    port = 8000+i
+    end = "0"
+    if i ==9:
+        end = "1"
+    subprocess.call(["python", "node.py", "--port", str(port), "--end", end])
