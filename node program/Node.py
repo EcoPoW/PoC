8370ef1c31d4a072435e15ecef39c3ab4375f311
EFFYLYX <effyli1997@gmail.com>
2018-07-04 00:00:31 +0800

    update

--- node program/Node.py
+++ node program/Node.py
@@ -1,8 +1,10 @@
-
 import tornado.web
 import tornado.websocket
 import tornado.ioloop
 import tornado.options
+
+from tornado import gen
+from tornado.websocket import websocket_connect
 
 
 class application(tornado.web.Application):
@@ -10,7 +12,7 @@ class application(tornado.web.Application):
         handlers = [(r"/", main_handler),
                     ]
         settings = dict(debug=True)
-        print("server starts")
+        print("port " + str(port) + " server starts")
 
         tornado.web.Application.__init__(self, handlers, **settings)
 
@@ -45,22 +47,51 @@ class main_handler(tornado.websocket.WebSocketHandler):
                 c.write_message(message)
 
 
-if __name__ == "__main__":
-    import argparse
+@gen.coroutine
+def read(to_url):
+    print("client trying to read msg")
+    webSocket = yield websocket_connect(to_url)
+    while True:
 
-    parser = argparse.ArgumentParser(description="progrom description")
+        msg = yield webSocket.read_message()
+        if msg is None:
+            break
+        else:
+            print(msg)
+            
+@gen.coroutine
+def write(to_url,msg):
+    print("client trying to write msg")
+    webSocket = yield websocket_connect(to_url)
+    webSocket.write_message(msg)
 
-    parser.add_argument('--port')
-    parser.add_argument('--end')
 
-    args = parser.parse_args()
-    port = args.port
-    end = args.end
 
-    print(str(port) + " " + str(end))
+import argparse
+
+parser = argparse.ArgumentParser(description="program description")
+
+parser.add_argument('--port')
+parser.add_argument('--end')
+parser.add_argument('--url')
+
+args = parser.parse_args()
+port = args.port
+end = args.end
+url = args.url
+
+# print(str(port) + " " + str(end))
+
+
+if url != "0":
+    to_url = "ws://localhost:" + url
+    print(to_url)
+    read(to_url)
+
+else:
 
     server = application()
-    server.listen(port)
+    server.listen(port)  # 比较一下listen和bind的区别
 
-    if end == "1":
-        tornado.ioloop.IOLoop.instance().start()
+if end == "1":
+    tornado.ioloop.IOLoop.instance().start()
