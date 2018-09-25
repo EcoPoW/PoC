aaee153d9d2d96cbec00ab426a353913e16d16f9
KJ <kernel1983@gmail.com>
2018-09-25 21:09:40 +0800

    fix simulation

--- gossip/simulation.py
+++ gossip/simulation.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 
 import random
+import copy
 
 
-SIZE = 50000
-CONTACT = 40
+SIZE = 10000
+# CONTACT = 100
 
 got_msg = set()
 user_friends = dict()
@@ -12,18 +13,29 @@ all_users = range(SIZE)
 
 for f in range(SIZE):
     random.shuffle(all_users)
-    friends = all_users[:CONTACT]
+    friends = copy.copy(all_users)
+    # friends = all_users[:CONTACT]
     # friends = list(set(friends) - set([f]))[:CONTACT]
     user_friends[f] = friends
     if not f % 100:
         print("progress:", f*100/SIZE)
 
-for i in range(CONTACT):
-    for f in user_friends:
-        friends = user_friends[f]
-        t = friends.pop(0)
-        user_friends[f] = friends
-        if t not in got_msg:
-            got_msg.add(t)
+got_msg.add(0)
+gossip_round = 1
+finish = False
+while not finish:
+    new_got_msg = set()
+    for i in got_msg:
+        friends = user_friends[i]
+        if friends:
+            t = friends.pop(0)
+            user_friends[i] = friends
+            if t not in got_msg:
+                new_got_msg.add(t)
 
-    print("round", i, "infect", len(got_msg))
+    print("round", gossip_round, "infect", len(got_msg))
+    if len(got_msg) == SIZE:
+        finish = True
+
+    got_msg = got_msg.union(new_got_msg)
+    gossip_round += 1
