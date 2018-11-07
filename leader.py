54da5cc3f713183388964f72ad71124d01636370
KJ <kernel1983@gmail.com>
2018-11-08 00:55:46 +0800

    update

--- leader.py
+++ leader.py
@@ -13,8 +13,72 @@ import tornado.escape
 import setting
 import tree
 import node
+import database
 
 working = False
+
+root_jump = {}
+def lastest_block(root_hash):
+    global root_jump
+
+    chains = []
+    prev_hashs = []
+    if root_hash in root_jump:
+        recent_hash = root_jump[root_hash]
+        chains.append([recent_hash])
+        prev_hashs.append(recent_hash)
+
+    else:
+        roots = database.connection.query("SELECT * FROM graph"+tree.current_port+" WHERE from_block = %s OR to_block = %s ORDER BY nonce", root_hash, root_hash)
+
+        for root in roots:
+            # print(root.id)
+            chains.append([root.hash])
+            prev_hashs.append(root.hash)
+
+    while True:
+        if prev_hashs:
+            prev_hash = prev_hashs.pop(0)
+        else:
+            break
+
+        leaves = database.connection.query("SELECT * FROM graph"+tree.current_port+" WHERE from_block = %s AND sender = %s ORDER BY nonce", prev_hash, root_hash)
+        if len(leaves) > 0:
+            for leaf in leaves:
+                # print(leaf.id)
+                for c in chains:
+                    if c[-1] == prev_hash:
+                        chain = c.copy()
+                        chain.append(leaf.hash)
+                        chains.append(chain)
+                        break
+                if leaf.hash not in prev_hashs and leaf.hash:
+                    prev_hashs.append(leaf.hash)
+
+        leaves = database.connection.query("SELECT * FROM graph"+tree.current_port+" WHERE to_block = %s AND receiver = %s ORDER BY nonce", prev_hash, root_hash)
+        if len(leaves) > 0:
+            for leaf in leaves:
+                # print(leaf.id)
+                for c in chains:
+                    if c[-1] == prev_hash:
+                        chain = c.copy()
+                        chain.append(leaf.hash)
+                        chains.append(chain)
+                        break
+                if leaf.hash not in prev_hashs and leaf.hash:
+                    prev_hashs.append(leaf.hash)
+
+    longest = []
+    for i in chains:
+        if not longest:
+            longest = i
+        if len(longest) < len(i):
+            longest = i
+    # print(longest)
+    if len(longest) > 3:
+        root_jump[root_hash] = longest[-3]
+    return longest
+
 
 def forward(seq):
     # global processed_message_ids
@@ -130,8 +194,19 @@ def mining():
     # global working
     # print(tree.current_port, "leader transactions", transactions)
     if transactions:
-        transaction = transactions.pop(0)
-        print(tree.current_port, transaction[1]["transaction"]["txid"])
+        seq = transactions.pop(0)
+        transaction = seq[1]
+        txid = transaction["transaction"]["txid"]
+        sender = transaction["transaction"]["sender"]
+        receiver = transaction["transaction"]["receiver"]
+        amount = transaction["transaction"]["amount"]
+        timestamp = transaction["transaction"]["timestamp"]
+        signature = transaction["signature"]
+
+        from_block = lastest_block(sender)
+        to_block = lastest_block(receiver)
+        print(tree.current_port, txid, from_block, to_block)
+
     if working:
         tornado.ioloop.IOLoop.instance().call_later(1, mining)
 
